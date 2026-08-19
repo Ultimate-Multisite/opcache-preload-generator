5f86a4f2464214e6d70d0bf7c3a3926489bae3c6
David Stone <david@nnucomputerwhiz.com>
2026-08-19 10:44:35 -0700

    chore: mark tested up to WordPress 7.1
    * chore: mark tested up to WordPress 7.1
    
    * fix: keep WordPress compatibility in readme only

--- opcache-preload-generator.php
+++ opcache-preload-generator.php
@@ -9,7 +9,6 @@
  * Author URI: https://multisiteultimate.com
  * Copyright: David Stone, Multisite Ultimate
  * Requires at least: 5.3
- * Tested up to: 7.0
  * Requires PHP: 7.4
  *
  * @package OPcache_Preload_Generator
